0d96eabf2a8403ada0e4df421b93f98ee52d6d29
Nguyendinhnam110204 <nguyendinhnam204211@gmail.com>
2024-06-29 00:34:42 +0700

    Update delete_MonHoc.php

--- MonHoc/delete_MonHoc.php
+++ MonHoc/delete_MonHoc.php
@@ -1,15 +1,21 @@
 <?php
-    //lay du lieu id can xoa
-    $idMonHoc = $_GET['idMonHoc'];
-    // echo $matheloai;
-    //ket noi
-    require_once '../connect.php';
+// Lấy dữ liệu id cần xóa
+$idMonHoc = $_GET['idMonHoc'];
+// Kết nối
+require_once '../connect.php';
 
-    //cau lenh sql
-    $delete_sql = "DELETE FROM monhoc WHERE idMonHoc = $idMonHoc";
+// Câu lệnh SQL
+$delete_sql = "DELETE FROM monhoc WHERE idMonHoc = $idMonHoc";
 
-    mysqli_query($conn, $delete_sql);
-
-    //tro ve trang list
+try {
+    if (!mysqli_query($conn, $delete_sql)) {
+        throw new Exception(mysqli_error($conn));
+    }
+    // Nếu xóa thành công, quay về trang list
     header("Location: index_MonHoc.php");
+} catch (Exception $e) {
+    // Nếu có lỗi, hiển thị thông báo và quay về trang list
+    echo '<script>alert("Không thể xóa do khóa ngoại"); window.location.href = "index_MonHoc.php";</script>';
+    exit();
+}
 ?>
